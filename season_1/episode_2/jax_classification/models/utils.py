e5ea7be2cb70e90ab398649e8d3a451e4beeb342
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2022-08-10 04:20:13 +0000

    added utilities for creating model and initializing model params

---
+++ season_1/episode_2/jax_classification/models/utils.py
@@ -0,0 +1,18 @@
+import jax
+import jax.numpy as jnp
+
+
+def create_model(model_class, apply_half_precision: bool, num_classes: int, **kwargs):
+    platform = jax.local_devices()[0].platform
+    dtype = jnp.flot32
+    if apply_half_precision:
+        dtype = jnp.bfloat16 if platform == "tpu" else jnp.float16
+    model = model_class(num_classes=num_classes, dtype=dtype, **kwargs)
+    return model
+
+
+def initialize_model(model_key: jnp.ndarray, image_size: int, model):
+    variables = jax.jit(model.init)(
+        {"params": model_key}, jnp.ones((1, image_size, image_size, 3), model.dtype)
+    )
+    return variables["params"], variables["batch_stats"]
